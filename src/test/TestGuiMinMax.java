c1cd04459d025b907d8b31cfee767b9f736d07f8
Raygunito <112569656+Raygunito@users.noreply.github.com>
2023-02-19 16:23:46 +0100

    Update TestGuiMinMax.java

--- src/test/TestGuiMinMax.java
+++ src/test/TestGuiMinMax.java
@@ -2,6 +2,6 @@ package test;
 
 public class TestGuiMinMax {
     public static void main(String[] args) {
-        GUIMinMax gui=new GUIMinMax();
+        TestMinMaxGUI gui=new TestMinMaxGUI();
     }
 }
