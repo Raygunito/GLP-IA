23495cd7977f6e5e9f53975a90fc6c6a5fc5d367
MagnusVonKarajan <84813233+MagnusVonKarajan@users.noreply.github.com>
2023-02-21 19:26:06 +0100

    slightly reworked Main_menu to make it slightly closer to our Figma

--- src/gui/Main_menu.java
+++ src/gui/Main_menu.java
@@ -8,12 +8,8 @@ import java.awt.geom.RoundRectangle2D;
 import java.io.File;
 import java.io.IOException;
 
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
+
 /**
  * Classe qui permet de créer le menu principal de notre application.
  */
@@ -28,13 +24,13 @@ public class Main_menu extends JPanel {
         init();
         add(Box.createVerticalStrut(50));
         add(title);
-        add(Box.createVerticalStrut(50));
+        add(Box.createVerticalStrut(40));
         add(astar);
-        add(Box.createVerticalStrut(10));
+        add(Box.createVerticalStrut(0));
         add(minmax);
-        add(Box.createVerticalStrut(10));
+        add(Box.createVerticalStrut(0));
         add(qlearn);
-        add(Box.createVerticalStrut(10));
+        add(Box.createVerticalStrut(0));
         add(quit);
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setPreferredSize(new Dimension(GUIConstant.DIM_X,GUIConstant.DIM_Y));
@@ -44,7 +40,7 @@ public class Main_menu extends JPanel {
         Font mainTitleFont = null;
         Font menuFont = null;
         try {
-            mainTitleFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/gui/fonts/poppins/Poppins-Bold.ttf")).deriveFont(36f);
+            mainTitleFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/gui/fonts/poppins/Poppins-Bold.ttf")).deriveFont(60f);
             menuFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/gui/fonts/poppins/Poppins-Bold.ttf")).deriveFont(22f);
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
             ge.registerFont(mainTitleFont);
@@ -57,25 +53,58 @@ public class Main_menu extends JPanel {
         title.setAlignmentX(CENTER_ALIGNMENT);
         title.setFont(mainTitleFont);
 
-        astar = new JButton("A Star");
+        ImageIcon astarimage = new ImageIcon("src/gui/assets/A-Star-button.png");
+        Image astarimage1 = astarimage.getImage();
+        Image newastar = astarimage1.getScaledInstance(300,45, Image.SCALE_SMOOTH);
+        astarimage = new ImageIcon(newastar);
+        astar = new JButton(astarimage);
         astar.setAlignmentX(CENTER_ALIGNMENT);
-        astar.setFont(menuFont);
-        astar.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.black, 0), BorderFactory.createEmptyBorder(10, 57, 10, 57)));
+        astar.setOpaque(false);
+        astar.setContentAreaFilled(false);
+        astar.setBorderPainted(false);
+        //astar.setFont(menuFont);
+        //astar.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.black, 0), BorderFactory.createEmptyBorder(10, 57, 10, 57)));
 
-        minmax = new JButton("MinMax");
+
+        ImageIcon minmaximage = new ImageIcon("src/gui/assets/MinMax-button.png");
+        Image minmaximage1 = minmaximage.getImage();
+        Image newminmax = minmaximage1.getScaledInstance(300,45, Image.SCALE_SMOOTH);
+        minmaximage = new ImageIcon(newminmax);
+        minmax = new JButton(minmaximage);
         minmax.setAlignmentX(CENTER_ALIGNMENT);
-        minmax.setFont(menuFont);
-        minmax.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.black, 0), BorderFactory.createEmptyBorder(10, 47, 10, 47)));
+        minmax.setOpaque(false);
+        minmax.setContentAreaFilled(false);
+        minmax.setBorderPainted(false);
+        //minmax.setFont(menuFont);
+        //minmax.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.black, 0), BorderFactory.createEmptyBorder(10, 47, 10, 47)));
 
-        qlearn = new JButton("QLearning");
+
+
+        ImageIcon qlearningimage = new ImageIcon("src/gui/assets/QLearning-button.png");
+        Image qlearningimage1 = qlearningimage.getImage();
+        Image newqlearning = qlearningimage1.getScaledInstance(300,45, Image.SCALE_SMOOTH);
+        qlearningimage = new ImageIcon(newqlearning);
+        qlearn = new JButton(qlearningimage);
         qlearn.setAlignmentX(CENTER_ALIGNMENT);
-        qlearn.setFont(menuFont);
-        qlearn.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.black, 0), BorderFactory.createEmptyBorder(10, 33, 10, 33)));
+        qlearn.setOpaque(false);
+        qlearn.setContentAreaFilled(false);
+        qlearn.setBorderPainted(false);
+        //qlearn.setFont(menuFont);
+        //qlearn.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.black, 0), BorderFactory.createEmptyBorder(10, 33, 10, 33)));
 
-        quit = new JButton("Quit");
+
+
+        ImageIcon quitterimage = new ImageIcon("src/gui/assets/Quitter-button.png");
+        Image quitterimage1 = quitterimage.getImage();
+        Image newquitter = quitterimage1.getScaledInstance(300,45, Image.SCALE_SMOOTH);
+        quitterimage = new ImageIcon(newquitter);
+        quit = new JButton(quitterimage);
         quit.setAlignmentX(CENTER_ALIGNMENT);
-        quit.setFont(menuFont);
-        quit.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.black, 0), BorderFactory.createEmptyBorder(10, 68, 10, 68)));
+        quit.setOpaque(false);
+        quit.setContentAreaFilled(false);
+        quit.setBorderPainted(false);
+        //quit.setFont(menuFont);
+        //quit.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.black, 0), BorderFactory.createEmptyBorder(10, 68, 10, 68)));
     }
 
     @Override
